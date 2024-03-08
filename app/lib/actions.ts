0588824bd7b0c19575357e1aa0f9ffcad58adb5c
Takayyz <like.the.wind0306@gmail.com>
2024-03-08 09:16:37 +0900

    feat: add store invoice logic w/o error handling

---
+++ app/lib/actions.ts
@@ -0,0 +1,41 @@
+'use server'
+
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({
+  id: true,
+  date: true,
+});
+
+export const createInvoice = async (formData: FormData) => {
+  const {customerId, amount, status} = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // INFO: to eliminate JavaScript floating-point errors
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  // TODO: Error handling
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amount}, ${status}, ${date})
+  `;
+
+  const INVOICES_PAGE_PATH = '/dashboard/invoices';
+  revalidatePath(INVOICES_PAGE_PATH);
+  redirect(INVOICES_PAGE_PATH);
+};
