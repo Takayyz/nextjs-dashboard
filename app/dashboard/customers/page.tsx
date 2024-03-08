493f25fdd4b187caf8e20dcaaa21ad2af0fa95a3
Takayyz <like.the.wind0306@gmail.com>
2024-03-08 21:42:21 +0900

    feat: impl fetch customers logic

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,21 @@
 import { Metadata } from "next";
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-const Page = () => (
-  <p>Customers page</p>
-);
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: {
+    query?: string;
+  },
+}) => {
+  const customers = await fetchFilteredCustomers(searchParams?.query || '');
+
+  return <Table customers={customers} />;
+};
 
 export default Page;
